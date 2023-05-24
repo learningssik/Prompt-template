dea44595ed796ab1b46fac5bc6cf489fc35edc7f
learningssik <learningssik@gmail.com>
2023-05-25 01:33:53 +0900

    example

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # Connecting to a template (html file)
-    mylist = [1,2,3,4,5]
+    mylist = [1,2,3,4,5,6]
     return render_template('00-Basic-Template.html', mylist = mylist)
 
 if __name__ == '__main__':
